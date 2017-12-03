db919604b83e11896eb0413f4eb187c0281521d4
Cameron Aziz <cameronaziz@gmail.com>
2017-12-02 16:27:38 -0800

    remove/update packages
    upgrade react, webpack, axios, react-dom; remove logrocket, enzyme,

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,9 +2,7 @@ import webpack from 'webpack';
 import path from 'path';
 
 export default {
-  debug: true,
   devtool: 'inline-source-map',
-  noInfo: false,
   entry: [
     'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
@@ -21,17 +19,17 @@ export default {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin()
   ],
   module: {
-    loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /(\.json)$/, loader: 'json-loader'},
-      {test: /\.css$/, loaders: ['style', 'css']},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    rules: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), use: [{ loader: 'babel-loader'}]},
+      {test: /(\.json)$/, use: [{loader: 'json-loader'}]},
+      {test: /\.css$/, use: [{loader: 'style'}, {loader: 'css'}]},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: [{loader: 'file'}]},
+      {test: /\.(woff|woff2)$/, use: [{loader: 'url?prefix=font/&limit=5000'}]},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: [{loader: 'url?limit=10000&mimetype=application/octet-stream'}]},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: [{loader: 'url?limit=10000&mimetype=image/svg+xml'}]}
     ]
   }
 };
